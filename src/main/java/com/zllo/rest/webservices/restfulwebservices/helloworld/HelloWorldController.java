6e2fca44a612438f6add3b81d9539aacc6c22da4
Gabriel <gabriel.conceicao@tivit.com>
2022-01-29 12:16:19 -0300

    Removing unused imports.

--- src/main/java/com/zllo/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/zllo/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -5,10 +5,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
